0a39c561247f47820dc333e7ff253a99657b8813
kimjeakwan <s24042@gsm.hs.kr>
2024-07-22 16:08:51 +0900

    basic

---
+++ src/main/java/com/example/ghskfen/rpdlrlagkwsn/GhskfenRpdlrlagkwsnApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghskfen.rpdlrlagkwsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhskfenRpdlrlagkwsnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhskfenRpdlrlagkwsnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ghskfen/rpdlrlagkwsn/GhskfenRpdlrlagkwsnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghskfen.rpdlrlagkwsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhskfenRpdlrlagkwsnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
