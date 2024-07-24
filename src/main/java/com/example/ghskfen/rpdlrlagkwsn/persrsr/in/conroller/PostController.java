15a92b545c4b1328ece4d510d88c2aa236913604
김한주 <s24043@gsm.hs.kr>
2024-07-24 19:13:28 +0900

    FEAT PostController PostRepository PostService

--- src/main/java/com/example/ghskfen/rpdlrlagkwsn/persrsr/in/conroller/PostController.java
+++ src/main/java/com/example/ghskfen/rpdlrlagkwsn/persrsr/in/conroller/PostController.java
@@ -1,22 +1,31 @@
 package com.example.ghskfen.rpdlrlagkwsn.persrsr.in.conroller;
 
 import com.example.ghskfen.rpdlrlagkwsn.persrsr.in.entity.Post;
-import com.example.ghskfen.rpdlrlagkwsn.service.Postservice;
+import com.example.ghskfen.rpdlrlagkwsn.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+
     @Autowired
-    private Postservice postservice;
+    private PostService postservice;
 
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody Post post) {
         Post createdPost = postservice.createPost(post);
         return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        if (postservice.deletPost(id)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/example/ghskfen/rpdlrlagkwsn/service/PostService.java
+++ src/main/java/com/example/ghskfen/rpdlrlagkwsn/service/PostService.java
@@ -2,16 +2,36 @@ package com.example.ghskfen.rpdlrlagkwsn.service;
 
 import com.example.ghskfen.rpdlrlagkwsn.persrsr.in.entity.Post;
 import com.example.ghskfen.rpdlrlagkwsn.persrsr.in.repository.PostRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Postservice {
+public class PostService {
+
     @Autowired
     private PostRepository postRepository;
+
 
     public Post createPost(Post post) {
         return postRepository.save(post);
     }
+
+
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public boolean deletPost(Long id) {
+        if (postRepository.existsById(id)) {
+            postRepository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Post not found with id " + id);
+        }
+        return false;
+    }
+
+
 }
 
