567d6770ef4772b04ff1a68564a4bad3125351df
김한주 <s24043@gsm.hs.kr>
2024-07-25 13:08:05 +0900

    FEAT application.yml build.gradle UserService

--- src/main/java/com/example/ghskfen/rpdlrlagkwsn/service/UserService.java
+++ src/main/java/com/example/ghskfen/rpdlrlagkwsn/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.ghskfen.rpdlrlagkwsn.service;
 
-import com.example.ghskfen.rpdlrlagkwsn.persrsr.in.repository.UserRepository;
+import com.example.ghskfen.rpdlrlagkwsn.persrsr.in.repository.PostRepository;
+
 import com.example.ghskfen.rpdlrlagkwsn.persrsr.model.UserRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private PostRepository userRepository;
 
     public void save(UserRequest user) {
         userRepository.save(user.toEntity());
